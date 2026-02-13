f13a81883234747dea52442192519b5c6f5587ea
Vasco Costa <vascomacosta@gmail.com>
2026-02-13 20:01:11 +0000

    Refactor leaderboard endpoint

--- src/api.rs
+++ src/api.rs
@@ -32,19 +32,21 @@ pub async fn leaderboard(
         .into_group_map_by(|g| &g.guess.username);
 
     let leaderboard = store.leaderboard(grouped_guesses).await;
-    let text_leaderboard: String = leaderboard
-        .iter()
-        .enumerate()
-        .map(|x| format!("{}. {} {}", x.0 + 1, x.1.0, x.1.1))
-        .join(" | ");
-    let json_leaderboard = Json(leaderboard);
 
     match format {
         Some(kind) => match kind {
-            "json" | "JSON" => Ok(LeaderboardResponse::Json(json_leaderboard)),
-            "text" | "TEXT" => Ok(LeaderboardResponse::Text(text_leaderboard)),
+            "json" | "JSON" => Ok(LeaderboardResponse::Json(Json(leaderboard))),
+            "text" | "TEXT" => {
+                let text_leaderboard: String = leaderboard
+                    .iter()
+                    .enumerate()
+                    .map(|x| format!("{}. {} {}", x.0 + 1, x.1.0, x.1.1))
+                    .join(" | ");
+
+                Ok(LeaderboardResponse::Text(text_leaderboard))
+            }
             _ => return Err(Status::InternalServerError),
         },
-        None => Ok(LeaderboardResponse::Json(json_leaderboard)),
+        None => Ok(LeaderboardResponse::Json(Json(leaderboard))),
     }
 }
